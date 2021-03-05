4a6c37d935a6785ca6472c567d917a05d84370a8
otherpirate <mauroamurari@gmail.com>
2021-03-05 13:40:29 -0300

    add profile info from contact info

--- pkg/models/profile.go
+++ pkg/models/profile.go
@@ -2,13 +2,49 @@ package models
 
 import "strings"
 
-type Profile struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-	Link string `json:"link"`
+type Address struct {
+	Line1      string `json:"line1"`
+	Line2      string `json:"line2"`
+	City       string `json:"city"`
+	State      string `json:"state"`
+	PostalCode string `json:"postal_code"`
+	Country    string `json:"country"`
 }
 
-func (p *Profile) SetIDLink(url string) {
-	p.Link = strings.ReplaceAll(url, "?viewMode=1", "")
-	p.ID = strings.ReplaceAll(p.Link, "https://www.upwork.com/freelancers/~", "")
+type Termination struct {
+	Date   string `json:"line1"`
+	Reason string `json:"line2"`
+}
+
+type Profile struct {
+	ID               string        `json:"id"`
+	Account          string        `json:"account"`
+	Employer         string        `json:"employer"`
+	CreatedAt        string        `json:"created_at"`
+	UpdatedAt        string        `json:"updated_at"`
+	FirstName        string        `json:"first_name"`
+	LastName         string        `json:"last_name"`
+	FullName         string        `json:"full_name"`
+	Email            string        `json:"email"`
+	PhoneNumber      string        `json:"phone_number"`
+	BirthDate        string        `json:"birth_date"`
+	PictureURL       string        `json:"picture_url"`
+	Address          Address       `json:"address"`
+	EmploymentStatus string        `json:"employment_status"`
+	EmploymentType   string        `json:"employment_type"`
+	JobTitle         string        `json:"job_title"`
+	SSN              string        `json:"ssn"`
+	PlatformUserID   string        `json:"platform_user_id"`
+	Terminations     []Termination `json:"terminations"`
+	MaritalStatus    string        `json:"marital_status"`
+	Gender           string        `json:"gender"`
+}
+
+func (p *Profile) SetNames(name string) {
+	p.FullName = name
+	names := strings.Split(name, " ")
+	p.FirstName = names[0]
+	if len(names) > 0 {
+		p.LastName = names[1]
+	}
 }
--- pkg/scrapping/profile.go
+++ pkg/scrapping/profile.go
@@ -2,6 +2,7 @@ package scrapping
 
 import (
 	"log"
+	"strings"
 
 	"github.com/anaskhan96/soup"
 	"github.com/otherpirate/upwork-scraping/pkg/models"
@@ -10,19 +11,77 @@ import (
 func (u *Upwork) Profile() (models.Profile, error) {
 	log.Println("Loading profile...")
 	profile := models.Profile{}
+	profile, err := u.loadProfileInfo(profile)
+	if err != nil {
+		return profile, err
+	}
+	profile, err = u.loadContactInfo(profile)
+	if err != nil {
+		return profile, err
+	}
+	log.Printf("Profiled loaded %s", profile.ID)
+	return profile, nil
+}
+
+func (u *Upwork) loadProfileInfo(profile models.Profile) (models.Profile, error) {
 	page, err := u.loadProfilePage()
 	if err != nil {
 		return profile, err
 	}
 	doc := soup.HTMLParse(page)
 	publicProfiliLink := doc.Find("div", "role", "group").Find("a")
-	profile.SetIDLink(publicProfiliLink.Attrs()["href"])
-	log.Printf("Profiled loaded %s", profile.ID)
+	profile.ID = cleanID(publicProfiliLink.Attrs()["href"])
 	return profile, nil
+}
+
+func (u *Upwork) loadContactInfo(profile models.Profile) (models.Profile, error) {
+	page, err := u.loadContactInfoPage()
+	if err != nil {
+		return profile, err
+	}
+	doc := soup.HTMLParse(page)
+	userID := doc.Find("div", "data-test", "userId")
+	name := doc.Find("div", "data-test", "userName")
+	email := doc.Find("div", "data-test", "userEmail")
+	phone := doc.Find("div", "data-test", "phone")
+	addressLine1 := doc.Find("span", "data-test", "addressStreet")
+	addressLine2 := doc.Find("span", "data-test", "addressStreet2")
+	addressCity := doc.Find("span", "data-test", "addressCity")
+	addressState := doc.Find("span", "data-test", "addressState")
+	addressPostalCode := doc.Find("span", "data-test", "addressZip")
+	addressCountry := doc.Find("span", "data-test", "addressCountry")
+	profile.PlatformUserID = Clean(userID.Text())
+	profile.SetNames(Clean(name.Text()))
+	profile.Email = Clean(email.Text())
+	profile.PhoneNumber = Clean(phone.Text())
+	profile.Address = models.Address{
+		Line1:      Clean(addressLine1.Text()),
+		Line2:      Clean(addressLine2.Text()),
+		City:       Clean(addressCity.Text()),
+		State:      Clean(addressState.Text())[2:],
+		PostalCode: Clean(addressPostalCode.Text()),
+		Country:    Clean(addressCountry.Text()),
+	}
+	return profile, nil
+}
+
+func cleanID(url string) string {
+	url = strings.ReplaceAll(url, "?viewMode=1", "")
+	return strings.ReplaceAll(url, "https://www.upwork.com/freelancers/~", "")
 }
 
 func (u *Upwork) loadProfilePage() (string, error) {
 	err := u.service.Navigate("https://www.upwork.com/freelancers")
+	if err != nil {
+		return "", err
+	}
+	return u.service.PageSource()
+}
+
+func (u *Upwork) loadContactInfoPage() (string, error) {
+	err := u.service.Navigate("https://www.upwork.com/freelancers/settings/contactInfo")
+	// TODO: Re-enter password
+	// TODO: Click Edit button to get email info
 	if err != nil {
 		return "", err
 	}
